0c892535a99bdf65dfe7ac2744bd71a40c1d1fd5
iyeongjun <they6687@naver.com>
2018-06-08 20:43:36 +0900

    Initial Commit

---
+++ SOPT_Github_Seminar_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOPT_Github_Seminar_iOS
+//
+//  Created by 이영준 on 2018. 6. 8..
+//  Copyright © 2018년 이영준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
