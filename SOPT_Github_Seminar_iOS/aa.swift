4747ef2c1a57d2226fc6e806ff56f5e0d6ef2041
kyeahen <bright19961@naver.com>
2018-06-09 15:55:48 +0900

    add test

---
+++ SOPT_Github_Seminar_iOS/aa.swift
@@ -0,0 +1,13 @@
+//
+//  aa.swift
+//  SOPT_Github_Seminar_iOS
+//
+//  Created by 김예은 on 2018. 6. 9..
+//  Copyright © 2018년 이영준. All rights reserved.
+//
+
+import Foundation
+
+struct aa {
+    let aa: String
+}
